c7d9c7692796b0f34695eccaf6ba01eecc4cafec
TWT <twt2333@outlook.com>
2020-10-16 23:46:40 +0800

    feat: test cmd: .b

--- src/cmd/b.py
+++ src/cmd/b.py
@@ -1,21 +1,18 @@
-import re
-
 from discord.ext import commands
 
 
 @commands.command(name='b')
-async def __base(ctx: commands.Context, *args):
-    count = len(args)
+async def action(ctx: commands.Context, *args):
+    print('[cmd] .b {}'.format(args))
 
-    dmg = ''
-    if count >= 1:
-        dmg = re.match(r'(\d+)[wW]?(\d+)?', args[0]).groups()
-        dmg = int(dmg[1]) * 10000 + (dmg[2] and int(dmg[2]) or 0)
+    dmg = len(args) >= 1 and args[0] or ''
 
-    time = count >= 2 and args[1] or ''
+    time = len(args) >= 2 and args[1] or ''
 
-    comments = count >= 3 and args[2] or ''
+    cmt = len(args) >= 3 and args[2] or ''
 
-    represent = count >= 4 and args[3] or ''
+    rep = len(args) >= 4 and '@' + args[3] or ''
+
+    await ctx.send('{} [{}] {} {}'.format(dmg, time, cmt, rep))
 
     pass
