fd74dfa7fbc0e6e72bc8e7916024624cff3b685d
TWT <twt2333@outlook.com>
2020-10-17 18:49:42 +0800

    feat: mention reped user when .b

--- src/cmd/b.py
+++ src/cmd/b.py
@@ -1,24 +1,25 @@
+from discord import Client
 from discord.ext import commands
 
 from doc import StatusSheet
 
-from .utils import get_name
+from member import get_name_from_id, get_id_from_name
 
 
 @commands.command(name='b')
 async def action(ctx: commands.Context, *args):
     print('[cmd] .b {}'.format(args))
 
-    dmg = len(args) >= 1 and args[0] or ''
+    dmg = len(args) > 0 and args[0] or ''
 
-    time = len(args) >= 2 and args[1] or ''
+    cmt = len(args) > 1 and args[1] or ''
 
-    cmt = len(args) >= 3 and args[2] or ''
+    rep = len(args) > 2 and args[2] or ''
 
-    rep = len(args) >= 4 and args[3] or ''
+    StatusSheet.update_b(get_name_from_id(ctx.author.id), dmg, cmt, rep)
 
-    StatusSheet.update_b(get_name(ctx.author.id), dmg, time, cmt, rep)
+    mention_rep = ctx.bot.get_user(get_id_from_name(rep)).mention
 
-    await ctx.send('{} 报刀完成: {} [{}] {} {}'.format(ctx.author.mention, dmg, time, cmt, rep))
+    await ctx.send('{} 报刀完成: {} "{}" {}'.format(ctx.author.mention, dmg, cmt, rep and mention_rep or ''))
 
     pass
