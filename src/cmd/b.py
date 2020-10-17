fb57558562ebf8b08d3249423b1b4a3f12394b89
TWT <twt2333@outlook.com>
2020-10-17 22:11:59 +0800

    style: code cleanup

--- src/cmd/b.py
+++ src/cmd/b.py
@@ -1,7 +1,6 @@
 from discord.ext import commands
 
 from doc import StatusSheet
-
 from member import get_name_from_id, get_id_from_name
 
 
@@ -10,15 +9,12 @@ async def action(ctx: commands.Context, *args):
     print('[cmd] .b {}'.format(args))
 
     dmg = len(args) > 0 and args[0] or ''
-
     cmt = len(args) > 1 and args[1] or ''
-
     rep = len(args) > 2 and args[2] or ''
 
     StatusSheet.update_b(get_name_from_id(ctx.author.id), dmg, cmt, rep)
 
-    mention_rep = ctx.bot.get_user(get_id_from_name(rep)).mention
-
-    await ctx.send('{} 报刀完成: {} "{}" {}'.format(ctx.author.mention, dmg, cmt, rep and mention_rep or ''))
+    await ctx.send('{} 报刀完成: {} "{}" {}'.format(ctx.author.mention, dmg, cmt,
+                                                rep and ctx.bot.get_user(get_id_from_name(rep)).mention or ''))
 
     pass
