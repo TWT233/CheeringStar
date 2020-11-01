6b6f126fac4f08d7266bb7a7db5c8742db78dc85
TWT <twt2333@outlook.com>
2020-11-01 22:56:54 +0800

    fix: add exception handling for member/utils

--- src/member/utils.py
+++ src/member/utils.py
@@ -1,17 +1,21 @@
 import json
 
+from exception import MemberNotFound
+
+members = json.load(open('../config/config.json', 'r', encoding='UTF-8'))['guild']['members']
+
 
 def get_user_from_id(user_id: int):
-    members = json.load(open('../config/config.json', 'r'))['guild']['members']
     for item in members:
         if item['id'] == user_id:
             return item
-    return {}
+    print('get_user_from_id failed')
+    raise MemberNotFound(user_id)
 
 
 def get_user_from_name(user_name: str):
-    members = json.load(open('../config/config.json', 'r'))['guild']['members']
     for item in members:
         if item['name'] == user_name:
             return item
-    return {}
+    print('get_user_from_name failed')
+    raise MemberNotFound(user_name)
