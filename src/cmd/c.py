6ae4e8486581204543ae0790f0d7792b675e3c37
TWT <twt2333@outlook.com>
2020-11-01 14:55:11 +0800

    chore: log modified

--- src/cmd/c.py
+++ src/cmd/c.py
@@ -1,13 +1,17 @@
 from discord.ext import commands
 
 from battle import *
+from doc import *
 
 
 @commands.command(name='c')
 async def action(ctx: commands.Context, *args):
-    print('[cmd] .c {}'.format(args))
+    print('[cmd] c {}'.format(args))
 
     dmg = len(args) > 0 and args[0] or ''
     rep = len(args) > 1 and args[1] or ''
 
     remain_time = Battle.commit(dmg)
+
+    if remain_time > 0:
+        tree_rip()
