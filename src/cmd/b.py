3cea7a011bd11121807279838a7c48dd13f050e2
TWT <twt2333@outlook.com>
2020-11-01 23:36:55 +0800

    fix: add exception handling for cmd/b

--- src/cmd/b.py
+++ src/cmd/b.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 
 from doc import StatusSheet
+from exception import MemberNotFound
 from member import get_user_from_id, get_user_from_name
 
 
@@ -12,9 +13,13 @@ async def action(ctx: commands.Context, *args):
     cmt = len(args) > 1 and args[1] or ''
     rep = len(args) > 2 and args[2] or ''
 
-    StatusSheet.update_b(get_user_from_id(ctx.author.id)['name'], dmg, cmt, rep)
+    try:
+        StatusSheet.update_b(get_user_from_id(ctx.author.id)['name'], dmg, cmt, rep)
 
-    await ctx.send('{} 报刀完成: {} "{}" {}'.format(ctx.author.mention, dmg, cmt,
-                                                rep and ctx.bot.get_user(get_user_from_name(rep)['id']).mention or ''))
-
-    pass
+        await ctx.send('{} 报刀完成: {} "{}" {}'.format(ctx.author.mention, dmg, cmt,
+                                                    rep and ctx.bot.get_user(
+                                                        get_user_from_name(rep)['id']).mention or ''))
+    except MemberNotFound:
+        return
+    else:
+        return
