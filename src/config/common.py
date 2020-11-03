2939ee69c7c2a9ddd71e13318d26233e3cea3d70
TWT <twt2333@outlook.com>
2020-11-04 00:37:15 +0800

    feat: config/common: manage common config
    usually named as 'config.json'

---
+++ src/config/common.py
@@ -0,0 +1,33 @@
+import json
+import os
+
+
+class Common:
+    # filename to config file
+    filename: str
+    # short of config
+    c: dict
+
+    def __init__(self, filename: str):
+        Common.filename = os.path.abspath(filename)
+        Common.c = json.load(open(filename, 'r', encoding='UTF-8'))
+
+    @staticmethod
+    def sync():
+        json.dump(Common.c, open(Common.filename, 'w', encoding='UTF-8'))
+
+    @staticmethod
+    def token() -> dict:
+        return Common.c['token']
+
+    @staticmethod
+    def sheets() -> dict:
+        return Common.c['sheets']
+
+    @staticmethod
+    def guild() -> dict:
+        return Common.c['guild']
+
+    @staticmethod
+    def boss() -> dict:
+        return Common.c['boss']
