0982453002bf1a4637ced935ebaa546876d730a7
TWT <twt2333@outlook.com>
2020-11-03 17:30:57 +0800

    feat: add cache mechanism for StatusSheet

--- src/doc/status.py
+++ src/doc/status.py
@@ -4,31 +4,43 @@ import gspread
 class StatusSheet:
     account: gspread.client.Client
     sheets: gspread.models.Spreadsheet
-    sheet: gspread.models.Worksheet
+    sheet: list
+    __sheet: gspread.models.Worksheet
+    __begin = 3
+    __end = 33
 
     def __init__(self, account_key: str, sheets_key: str, sheet_title: str):
         StatusSheet.account = gspread.service_account(account_key)
         StatusSheet.sheets = StatusSheet.account.open_by_key(sheets_key)
-        StatusSheet.sheet = StatusSheet.sheets.worksheet(sheet_title)
+        StatusSheet.__sheet = StatusSheet.sheets.worksheet(sheet_title)
+        StatusSheet.sheet = StatusSheet.__sheet.get_all_values()
         pass
 
     @staticmethod
+    def sheet_update(range_name, values=None, **kwargs):
+        StatusSheet.__sheet.update(range_name, values, kwargs)
+        StatusSheet.sheet = StatusSheet.__sheet.get_all_values()
+
+    @staticmethod
+    def sheet_batch_update(data, **kwargs):
+        StatusSheet.__sheet.batch_update(data, kwargs)
+        StatusSheet.sheet = StatusSheet.__sheet.get_all_values()
+
+    @staticmethod
     def update_b(exe: str, dmg: str, cmt: str, rep: str):
-        sheet = StatusSheet.sheet.get_all_values()
-        for i in range(len(sheet)):
-            if sheet[i][0] == (rep or exe):
-                StatusSheet.sheet.update('B{}:D{}'.format(i + 1, i + 1), [[dmg, cmt, rep and exe or '']])
+        for i in range(len(StatusSheet.sheet)):
+            if StatusSheet.sheet[i][3] == (rep or exe):
+                StatusSheet.sheet_update('B{}:D{}'.format(i + 1, i + 1), [[dmg, cmt, rep and exe or '']])
 
     @staticmethod
     def call_clean():
-        sheet = StatusSheet.sheet
-        sheet_data = sheet.get_all_values()
         update_list = []
 
-        for i in range(1, min(len(sheet_data), 31)):
-            if sheet_data[i][1] or sheet_data[i][2] or sheet_data[i][3]:
-                update_list.append({'range': 'B{}:D{}'.format(i + 1, i + 1),
-                                    'values': [['', '', '']]})
+        for i in range(StatusSheet.__begin, min(len(StatusSheet.sheet), StatusSheet.__end)):
+            if StatusSheet.sheet[i][0] or StatusSheet.sheet[i][4] or StatusSheet.sheet[i][5]:
+                update_list.append({'range': 'A{}'.format(i + 1), 'values': [['']]})
+                update_list.append({'range': 'E{}'.format(i + 1), 'values': [['']]})
+                update_list.append({'range': 'F{}'.format(i + 1), 'values': [['']]})
 
-        sheet.batch_update(update_list)
+        StatusSheet.sheet_batch_update(update_list)
         return
