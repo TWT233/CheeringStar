2c87bc2d3042870ddd33b1b450a8fdb1c091b1c6
TWT <twt2333@outlook.com>
2020-11-03 17:29:32 +0800

    feat: cmd/cs: chashu

---
+++ src/cmd/cs.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+
+import doc
+
+
+@commands.command(name='cs')
+async def action(ctx: commands.Context):
+    print('[cmd] cs {}'.format(ctx.author.name))
+
+    shu_list = doc.StatusSheet.get_shu()
+
+    ret = "樹上傷害："
+
+    for i in shu_list:
+        ret += "\n" + i['name'] + " : " + i['dmg'] + " : " + i['cmt']
+
+    await ctx.send(ctx.author.mention + ret)
+    return
