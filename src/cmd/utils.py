43de5554d2ad5c58bd12970e8b43968508865904
TWT <TWT2333@outlook.com>
2020-11-20 20:30:49 +0800

    fix: missing * and ** in passing args

--- src/cmd/utils.py
+++ src/cmd/utils.py
@@ -5,6 +5,6 @@ def log(func):
     @functools.wraps(func)
     async def wrapper(*args, **kwargs):
         print('[cmd] ' + ','.join(["\'" + i + "\'" for i in args]))
-        return func(args, kwargs)
+        return func(*args, **kwargs)
 
     return wrapper
