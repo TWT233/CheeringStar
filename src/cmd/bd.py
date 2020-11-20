cfbde9fb0d9c684221766bf6ec667d90c3f1bb62
TWT <TWT2333@outlook.com>
2020-11-20 20:22:02 +0800

    feat: add exception handling

--- src/cmd/bd.py
+++ src/cmd/bd.py
@@ -3,23 +3,24 @@ from discord.ext import commands
 from doc import StatusSheet
 from exception import MemberNotFound
 from member import get_user_from_id, get_user_from_name
+from .utils import log
 
 
-@commands.command(name='bd', aliases=['報'])
-async def action(ctx: commands.Context, *args):
-    print('[cmd] bd {} {}'.format(ctx.author.name, args))
-
-    dmg = len(args) > 0 and args[0] or ''
-    cmt = len(args) > 1 and args[1] or ''
-    rep = len(args) > 2 and args[2] or ''
+@commands.command(name='bd', aliases=['報', '报'])
+@log
+async def action(ctx: commands.Context, dmg, cmt, *args):
+    rep = len(args) > 0 and args[0] or ''
 
     try:
-        StatusSheet.update_b(get_user_from_id(ctx.author.id)['name'], dmg, cmt, rep)
+        exe = get_user_from_id(ctx.author.id)
+        rep_user = get_user_from_name(rep)
 
-        await ctx.send('{} 报刀完成: {} "{}" {}'.format(ctx.author.mention, dmg, cmt,
-                                                    rep and ctx.bot.get_user(
-                                                        get_user_from_name(rep)['id']).mention or ''))
-    except MemberNotFound:
+        StatusSheet.update_b(exe['name'], dmg, cmt, rep)
+        await ctx.send('{} 报刀完成: {} "{}" {}'.format(
+            ctx.author.mention, dmg, cmt, ctx.bot.get_user(rep_user['id']).mention))
+    except MemberNotFound as e:
+        await ctx.send("未找到成员：" + e.expecting)
+    finally:
         return
 
 
