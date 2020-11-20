3113894f15f338c933a656bf59ff9adca0455588
TWT <TWT2333@outlook.com>
2020-11-20 20:17:24 +0800

    feat: decorator: log: log cmd info

---
+++ src/cmd/utils.py
@@ -0,0 +1,10 @@
+import functools
+
+
+def log(func):
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        print('[cmd] ' + ','.join(["\'" + i + "\'" for i in args]))
+        return func(args, kwargs)
+
+    return wrapper
