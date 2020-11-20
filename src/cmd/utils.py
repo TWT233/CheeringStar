ee42bf02e72ec3ae34ddeafd20564269a1e0f69e
TWT <TWT2333@outlook.com>
2020-11-20 21:03:39 +0800

    fix: cant cast ctx to str

--- src/cmd/utils.py
+++ src/cmd/utils.py
@@ -4,7 +4,7 @@ import functools
 def log(func):
     @functools.wraps(func)
     async def wrapper(*args, **kwargs):
-        print('[cmd] ' + ','.join(["\'" + i + "\'" for i in args]))
-        return func(*args, **kwargs)
+        print('[cmd] ' + args[0].message.content)
+        return await func(*args, **kwargs)
 
     return wrapper
