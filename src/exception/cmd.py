f315195349fae16be8f28679be38b2a312dd3440
TWT <twt2333@outlook.com>
2020-11-21 00:06:39 +0800

    feat(exception/cmd): store exception info

--- src/exception/cmd.py
+++ src/exception/cmd.py
@@ -1,3 +1,4 @@
 class ArgumentNotEnough(Exception):
     def __init__(self, exact, excepting):
-        print('ArgumentNotEnough: got {}, expecting {}'.format(exact, excepting))
+        self.exact = exact
+        self.excepting = excepting
