03f9d3c203ece259deeddaaa5f1b809f5e9b379a
TWT <TWT2333@outlook.com>
2020-11-20 21:04:27 +0800

    fix: cast type

--- src/exception/member.py
+++ src/exception/member.py
@@ -1,4 +1,4 @@
 class MemberNotFound(Exception):
     def __init__(self, expecting):
-        self.expecting: str = expecting
+        self.expecting: str = str(expecting)
         print('MemberNotFound: {}'.format(expecting))
