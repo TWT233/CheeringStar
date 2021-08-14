3d30d34bc295c28ac464e7290ce73a2451972b52
TWT <twt2333@outlook.com>
2021-08-14 14:42:23 +0800

    feat(config): save_conf()

--- src/config.py
+++ src/config.py
@@ -9,3 +9,7 @@ CONF_DIR = os.path.join(PROJ_DIR, 'conf')  # '/conf'
 
 # load config file
 conf = yaml.load(open(os.path.join(CONF_DIR, 'config.yaml'), encoding='utf-8'), Loader=yaml.FullLoader)
+
+
+async def save_conf():
+    yaml.dump(conf, open(os.path.join(CONF_DIR, 'config.yaml'), 'w', encoding='utf-8'))
